b3122974f479fd8ea1297378682e8298591cd107
FerdinandL <fra.lgs@gmail.com>
2016-08-18 18:42:21 -0400

    Update 2016/08/18

--- Tutorials/Python/Python_Formulas.py
+++ Tutorials/Python/Python_Formulas.py
@@ -10,8 +10,8 @@ Created on Tue May 24 16:32:15 2016
 #create dataframe
 
 # Importing dataframe from/to csv
-taxi500 = pd.read_csv('C:\Users\Ferdinand\Documents\NYU\Data\taxi500.csv')
-df.to_csv(path='path/to/file',sep=',',header=true,columns=['latitude','longitude'])
+taxi500 = pd.read_csv('C:\Users\Ferdinand\Documents\NYU\Data\taxi500.csv', header = 0, names=["node_id","count","datetime"], sep = ",",index_col=False)
+df.to_csv(path='path/to/file',sep=',',header= True, columns=['latitude','longitude'], index=False)
 
 # Manipulating dataframes
 df.loc[df['column_name'] == some_value] # select rows
@@ -47,6 +47,14 @@ coord_indexed = coord.set_index(coord['cell_id'])
 # sort by columns
 result = df.sort(['c','d'], ascending=[1,0])
 
+# Change element type of column
+df[columns] = df[columns].astype('float32')
+counts['count'] = pd.to_numeric(counts['count'])
+df[['col2','col3']] = df[['col2','col3']].apply(pd.to_numeric)
+
+# Filter rows with value lists
+df[df['col1'].isin([3, 6])]
+
 ## -------- grouping and summarizing data ----------
 bygroup_treatment = data.groupby(['Group', 'Treatment']) #grouping according to two cols
 bygroup_treatment = data.groupby(['Group', 'Treatment'], as_index=False) # to prevent columns becoming index
@@ -78,6 +86,25 @@ counts1110.write("cell_id, count, date \n")
 counts1110.close()
 taxi.readline()
 
+#Copying file input in copyFile
+copyFile = open("filename.txt", "w")
+inputFile = open("inputfilename.txt")
+for line in inputFile:
+    copyFile.write(line)
+    
+#Inserting text in text file in place
+import fileinput
+hola = open("/home/ferdinand/Documents/NYU/satscan/xp_roads_elp/iterative/hola.txt","w")
+hola.write("hola1\n")
+hola.write("hola2\n")
+hola.write("hola3\n")
+hola.close()
+
+for line in fileinput.input("/home/ferdinand/Documents/NYU/satscan/xp_roads_elp/iterative/hola.txt", inplace = 1):      
+    print line.replace("\n","")    
+    if (line[4] == str(2)):
+        print "bonjour"
+
 #%% Strings & text processing
 
 # String manipulation
@@ -102,3 +129,11 @@ list1.append(['a','g','h','j'])
 
 # check if value is in list
 value in list1
+
+# convert all strings of a list to int values
+results_list = ['1', '3', '4']
+results_list = map(int, results_list)
+
+## --------------- running command lines from python script -----------------
+from subprocess import call
+call(["ls", "-l"])
